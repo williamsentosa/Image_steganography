b6b1aa1f877f4275ea3a53257a5a908f0567ec53
William Sentosa <williamsentosa.sentosa@gmail.com>
2016-02-15 19:51:32 +0700

    added image class

---
+++ Image Steganography/src/image/Image.java
@@ -0,0 +1,72 @@
+package image;
+
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author William Sentosa
+ */
+public class Image {
+    private String path;
+
+    public Image() {
+        // do nothing
+    }
+    
+    public Image(String path) {
+        this.path = path;
+    }
+    
+    /**
+     * Setting image path
+     * @param path image path
+     */
+    public void setImage(String path) {
+        this.path = path;
+    }
+    
+    /**
+     * Convert image with the set path into bytes
+     * @return bytes of image
+     */
+    public byte[] extractByte() {
+        ByteOutputStream bos = null;
+        try {
+            File imgPath = new File(path);
+            BufferedImage bufferedImage = ImageIO.read(imgPath);
+            try {
+                bos = new ByteOutputStream();
+                ImageIO.write(bufferedImage, "png", bos);
+            } catch (IOException ex) {
+                Logger.getLogger(Image.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                try {
+                    bos.close();
+                } catch (Exception e) {
+                    
+                }
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Image.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return bos == null ? null : bos.getBytes();
+    }
+    
+    public static void main(String args[]) {
+        String path = "Mushroom.png";
+        Image image = new Image(path);
+        byte[] bytes = image.extractByte();
+        System.out.println(Arrays.toString(bytes));
+    }
+    
+}
