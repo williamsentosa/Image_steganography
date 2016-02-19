c40d7d1cd3e43f16894be0f339a7404a077562ef
William Sentosa <williamsentosa.sentosa@gmail.com>
2016-02-19 16:29:51 +0700

    added ByteProcessor class

---
+++ Image Steganography/src/processor/ByteProcessor.java
@@ -0,0 +1,23 @@
+package processor;
+
+/**
+ *
+ * @author William Sentosa
+ */
+public class ByteProcessor {
+    private byte[][] bytes;
+    
+    public ByteProcessor() {
+        bytes = null;
+    }
+    
+    public ByteProcessor(byte[] b) {
+        for(int i=0; i<b.length; i++) {
+            
+        }
+    }
+    
+    public void setBytes(byte[] b) {
+        
+    }
+}
