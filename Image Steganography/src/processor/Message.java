1746f29b9eeae58cd15fea193ca07afdc609ac6b
angelynz95 <angel_lynz95@hotmail.com>
2016-02-26 20:26:31 +0700

    Added method to convert bitplanes to message bytes

--- Image Steganography/src/processor/Message.java
+++ Image Steganography/src/processor/Message.java
@@ -1,5 +1,6 @@
 package processor;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -25,33 +26,44 @@ public class Message {
     
     public Message(int length) {
         this.length = length;
+        message = new byte[this.length];
     }
     
     public void setMessage(String path) throws IOException {
         Path filePath = Paths.get(path);
         message = Files.readAllBytes(filePath);
+        length = message.length;
+    }
+    
+    public void setLength(int length) {
+        this.length = length;
+        message = new byte[this.length];
     }
     
     public byte[] getMessage() {
         return message;
     }
     
+    public int getLength() {
+        return length;
+    }
+    
     public Bitplane[] convertToBitplane() {
-        Bit[][] bits = new Bit[message.length][8];
+        Bit[][] bits = new Bit[length][8];
         Bitplane[] bitplane;
         ByteConverter byteConverter = new ByteConverter();
         
         // Mengubah semua byte ke bit
-        for (int i = 0; i < message.length; i++) {
+        for (int i = 0; i < length; i++) {
             bits[i] = byteConverter.convertByteToBits(message[i]);
         }
         // Mengubah kumpulan bit menjadi kumpulan bitplane
-        if ((message.length % 64) == 0) {
-            bitplane = new Bitplane[(message.length / 64) * 8];
+        if ((length % 64) == 0) {
+            bitplane = new Bitplane[(length / 64) * 8];
         } else {
-            bitplane = new Bitplane[((message.length / 64) + 1) * 8];
+            bitplane = new Bitplane[((length / 64) + 1) * 8];
         }
-        for (int i = 0; i < ((message.length / 64) * 8); i++) {
+        for (int i = 0; i < ((length / 64) * 8); i++) {
             bitplane[i] = new Bitplane();
             for (int j = 0; j < 8; j++) {
                 for (int k = 0; k < 8; k++) {
@@ -59,23 +71,23 @@ public class Message {
                 }
             }
         }
-        if ((message.length % 64) != 0) {
+        if ((length % 64) != 0) {
             // Ada bitplane yang memiliki elemen dummy
-            for (int i = ((message.length / 64) * 8); i < ((message.length / 64)*8 + 8); i++) {
+            for (int i = ((length / 64) * 8); i < ((length / 64)*8 + 8); i++) {
                 bitplane[i] = new Bitplane();
-                for (int j = 0; j < ((message.length % 64) / 8); j++) {
+                for (int j = 0; j < ((length % 64) / 8); j++) {
                     for (int k = 0; k < 8; k++) {
                         bitplane[i].setBitsBasedOnPosition(j, k, bits[(i / 8)*64 + j*8 +k][i % 8]);
                     }
                 }
-                for (int j = 0; j < ((message.length % 64) % 8); j++) {
-                    bitplane[i].setBitsBasedOnPosition((message.length % 64) / 8, j, bits[(i / 8)*64 + ((message.length % 64) / 8)*8 +j][i % 8]);
+                for (int j = 0; j < ((length % 64) % 8); j++) {
+                    bitplane[i].setBitsBasedOnPosition((length % 64) / 8, j, bits[(i / 8)*64 + ((length % 64) / 8)*8 +j][i % 8]);
                 }
                 // Mengisi elemen dummy
-                for (int j = ((message.length % 64) % 8); j < 8; j++) {
-                    bitplane[i].setBitsBasedOnPosition((message.length % 64) / 8, j, new Bit());
+                for (int j = ((length % 64) % 8); j < 8; j++) {
+                    bitplane[i].setBitsBasedOnPosition((length % 64) / 8, j, new Bit());
                 }
-                for (int j = ((message.length % 64) / 8 + 1); j < 8; j++) {
+                for (int j = ((length % 64) / 8 + 1); j < 8; j++) {
                     for (int k = 0; k < 8; k++) {
                         bitplane[i].setBitsBasedOnPosition(j, k, new Bit());
                     }
@@ -98,14 +110,33 @@ public class Message {
     }
     
     public void deconvertFromBitplane(Bitplane[] bitplanes) {
-        Bit[] bits;
+        Bit[][] bits = new Bit[length][8];
         ByteConverter byteConverter = new ByteConverter();
         
-//        for (int i = 0; i < bitplanes.length; i++) {
-//            for (int j = 0; j < bitplanes)
-//        }
-//        
-//        message = byteConverter.convertBitsToByte(bits);
+        for (int i = 0; i < ((length / 64) * 8); i++) {
+            for (int j = 0; j < 8; j++) {
+                for (int k = 0; k < 8; k++) {
+                    bits[(i / 8)*64 + j*8 +k][i % 8] = bitplanes[i].getBitsBasedOnPosition(j, k);
+                }
+            }
+        }
+        if ((length % 64) != 0) {
+            // Ada bitplane yang memiliki elemen dummy
+            for (int i = ((length / 64) * 8); i < ((length / 64)*8 + 8); i++) {
+                for (int j = 0; j < ((length % 64) / 8); j++) {
+                    for (int k = 0; k < 8; k++) {
+                        bits[(i / 8)*64 + j*8 +k][i % 8] = bitplanes[i].getBitsBasedOnPosition(j, k);
+                    }
+                }
+                for (int j = 0; j < ((length % 64) % 8); j++) {
+                    bits[(i / 8)*64 + ((length % 64) / 8)*8 +j][i % 8] = bitplanes[i].getBitsBasedOnPosition((length % 64) / 8, j);
+                }
+            }
+        }
+        
+        for (int i = 0; i < length; i++) {
+            message[i] = byteConverter.convertBitsToByte(bits[i]);
+        }
     }
     
     public void encrypt() {
@@ -119,14 +150,15 @@ public class Message {
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
         Message message = new Message("C:\\Users\\Windows7\\Desktop\\Kripto\\teks.txt");
-        message.convertToBitplane();
+        Message message2 = new Message(message.getLength());
+        message2.deconvertFromBitplane(message.convertToBitplane());
 //        File file = new File("C:\\Users\\Windows7\\Desktop\\Kripto\\doc.docx");
 //        MimeUtil.registerMimeDetector("eu.medsea.mimeutil.detector.MagicMimeMimeDetector");
 //        MimeType mimeType = MimeUtil.getMostSpecificMimeType(MimeUtil.getMimeTypes(file));
 //        MimeUtil.unregisterMimeDetector("eu.medsea.mimeutil.detector.MagicMimeMimeDetector");
 //        System.out.println(mimeType.toString());
-//        FileOutputStream fos = new FileOutputStream("C:\\Users\\Windows7\\Desktop\\Kripto\\tes2");
-//        fos.write(message.getMessage());
+//        FileOutputStream fos = new FileOutputStream("C:\\Users\\Windows7\\Desktop\\Kripto\\newteks.txt");
+//        fos.write(message2.getMessage());
 //        fos.close();
     }
     
