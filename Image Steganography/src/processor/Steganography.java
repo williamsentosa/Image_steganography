58110454844bab4b2757aeecd3e2e09b9a15ecde
William Sentosa <williamsentosa.sentosa@gmail.com>
2016-02-26 21:08:55 +0700

    clean up

--- Image Steganography/src/processor/Steganography.java
+++ Image Steganography/src/processor/Steganography.java
@@ -30,35 +30,35 @@ public class Steganography {
     }
     
     public void hideInformation(Message message, String key) {
-        ArrayList<Bitplane[]> listBitplanes = new ArrayList<>();
-        image.getBytesFromImage();
-        Block[][] blocks = image.convertBytesToBlocks();
-        int bitSize = 0;
-        for(int i=0; i<blocks.length; i++) {
-            for(int j=0; j<blocks[i].length; j++) {
-                //listBitplanes.add(blocks[i][j].convertToBitplanes());
-            }
-        }
-        Bitplane[] messages = message.convertToBitplane();
-        int n = 0;
-        double threshold = 0.3;
-        
-        for(int j=0; j<listBitplanes.size(); j++) {
-            for(int i=0; i<listBitplanes.get(j).length; i++) {
-                listBitplanes.get(j)[i].convertToCGC();
-                if(listBitplanes.get(j)[i].isNoisy(0.3) && n < messages.length) {
-                    if(messages[n].isNoisy(threshold)) {
-                        table.addSign(new Bit(false));
-                        listBitplanes.get(j)[i] = messages[n];
-                    } else {
-                        messages[n].conjugate();
-                        listBitplanes.get(j)[i] = messages[n];
-                        table.addSign(new Bit(true));
-                    }
-                    n++;
-                }
-            }
-        }
+//        ArrayList<Bitplane[]> listBitplanes = new ArrayList<>();
+//        image.getBytesFromImage();
+//        Block[][] blocks = image.convertBytesToBlocks();
+//        int bitSize = 0;
+//        for(int i=0; i<blocks.length; i++) {
+//            for(int j=0; j<blocks[i].length; j++) {
+//                //listBitplanes.add(blocks[i][j].convertToBitplanes());
+//            }
+//        }
+//        Bitplane[] messages = message.convertToBitplane();
+//        int n = 0;
+//        double threshold = 0.3;
+//        
+//        for(int j=0; j<listBitplanes.size(); j++) {
+//            for(int i=0; i<listBitplanes.get(j).length; i++) {
+//                listBitplanes.get(j)[i].convertToCGC();
+//                if(listBitplanes.get(j)[i].isNoisy(0.3) && n < messages.length) {
+//                    if(messages[n].isNoisy(threshold)) {
+//                        table.addSign(new Bit(false));
+//                        listBitplanes.get(j)[i] = messages[n];
+//                    } else {
+//                        messages[n].conjugate();
+//                        listBitplanes.get(j)[i] = messages[n];
+//                        table.addSign(new Bit(true));
+//                    }
+//                    n++;
+//                }
+//            }
+//        }
         // Belom disimpen informasinya ke conjugation table
         // Belom disimpen bitplane informasi ke akhir image.
         // Belom di deconvert jadi pbc lagi
